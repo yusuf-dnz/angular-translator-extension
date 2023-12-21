5ecd4f321bd9458db331d381ac703a4733bd3f60
Yusuf Deniz <joseph__sea@hotmail.com>
2023-12-21 15:47:04 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TranslatorPageComponent } from './translator-page/translator-page.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,TranslatorPageComponent],
+  imports: [CommonModule, RouterOutlet, TranslatorPageComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/translator-page/translator-page.component.ts
+++ src/app/translator-page/translator-page.component.ts
@@ -1,31 +1,58 @@
 import { Component } from '@angular/core';
 import { TranslatorApiService } from '../utils/translator-api.service';
 import { Observable } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-translator-page',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './translator-page.component.html',
   styleUrl: './translator-page.component.css',
 })
 export class TranslatorPageComponent {
-  translatedText = Observable<any>;
+  translatedDataObject: Array<any> = [];
+  translatedValue = { text: '', to: '' };
+  textAreaValue: string = '';
+  translateOptions: Array<any> = [
+    { id: 0, from: '', to: 'en', description: 'Oto - EN' },
+    { id: 1, from: 'en', to: 'tr', description: 'EN - TR' },
+  ];
+  option: number = 0;
+
   constructor(private translatorService: TranslatorApiService) {}
-  ngOnInit() {
+
+  translate() {
+    this.translatorService
+      .translator(
+        this.textAreaValue,
+        this.translateOptions[this.option].from,
+        this.translateOptions[this.option].to
+      )
+      .subscribe(
+        (response) => {
+          this.translatedDataObject = response;
+        },
+        (error) => {
+          console.error(error);
+        },
+        () => {
+          this.translatedValue.text =
+            this.translatedDataObject[0].translations[0].text;
+          this.translatedValue.to =
+            this.translatedDataObject[0].translations[0].to;
+          console.log(this.translatedValue.text, this.translatedValue.to);
+        }
+      );
+  }
+
+  translateFunc() {
     this.translate();
   }
-  translate() {
-    this.translatorService.translator('Hello', 'en', 'tr').subscribe(
-      (response) => {
-        this.translatedText = response;
-      },
-      (error) => {
-        console.error(error);
-      },
-      () => {
-        console.log('Çeviri sonucu' + this.translatedText);
-      }
-    );
+  changeOption(option: number) {
+    // İki kez çalışıyor
+    this.option = option;
+    console.log(this.option)
   }
 }
